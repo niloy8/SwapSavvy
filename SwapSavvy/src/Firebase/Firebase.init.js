f54a2d0f838fe4e8c3b9461d909097523db66f45
niloy8 <niloybhuiyan688@gmail.com>
2025-07-25 14:27:53 +0600

    Firebase Initialized

---
+++ SwapSavvy/src/Firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCfJMXOiqQkwB9oMBS38QsGkXTfZ_sLvw8",
+    authDomain: "swapsavvy-ae918.firebaseapp.com",
+    projectId: "swapsavvy-ae918",
+    storageBucket: "swapsavvy-ae918.firebasestorage.app",
+    messagingSenderId: "906460508688",
+    appId: "1:906460508688:web:1044f97b7cf8de7e799095"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
